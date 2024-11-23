0e1613523d3735375cb25b38ac0883a343568c14
Harjit <lidder.harjit@gmail.com>
2024-11-23 01:09:04 +0100

    Ajout nouvelle fonction de merge

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 use core::alloc::{GlobalAlloc, Layout};
-use core::ptr::{null_mut};
+use core::ptr::null_mut;
 use core::panic::PanicInfo;
 use core::cell::UnsafeCell;
 
@@ -13,9 +13,9 @@ fn panic(_info: &PanicInfo) -> ! {
 
 // Bloc de mémoire libre
 #[repr(C)]
-struct Block {
-    size: usize,              // Taille du bloc
-    next: *mut Block,         // Pointeur vers le prochain bloc
+pub struct Block {
+    pub size: usize,              // Taille du bloc
+    pub next: *mut Block,         // Pointeur vers le prochain bloc
 }
 
 impl Block {
@@ -39,33 +39,38 @@ unsafe impl Sync for FreeListAllocator {}
 
 unsafe impl GlobalAlloc for FreeListAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let mut current = *self.free_list.get(); // Récupère la liste des blocs libres
-        let mut previous_block: *mut Block = null_mut(); // Pointeur vers le bloc précédent
+        let (size, align) = (layout.size(), layout.align());
+        let allocator = self.lock();
 
-        while !current.is_null() {
-            if (*current).size >= layout.size() {
-                if !previous_block.is_null() {
-                    (*previous_block).next = (*current).next;
-                } else {
-                    *self.free_list.get() = (*current).next;
-                }
+        if let Some((region, alloc_start)) = allocator.find_block(size, align) {
+            let alloc_end = alloc_start.checked_add(size).expect("Overflow");
+            let excess_size = (*region).finishing_addr() - alloc_end;
 
-                return (*current).starting_addr() as *mut u8;
+            if excess_size > 0 {
+                allocator.insert_free_region(alloc_end, excess_size);
             }
 
-            previous_block = current;
-            current = (*current).next;
+            alloc_start as *mut u8
+        } else {
+            null_mut()
         }
-
-        null_mut()
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        self.insert_free_region(ptr as usize, layout.size());
+        let size = layout.size();
+        let allocator = self.lock();
+        allocator.insert_free_region(ptr as usize, size);
+        allocator.merge_free_blocks();
     }
 }
 
 impl FreeListAllocator {
+    /// Retourne une référence mutable à l'allocateur en utilisant `UnsafeCell`
+    pub unsafe fn lock(&self) -> &mut FreeListAllocator {
+        // Utilisation correcte de `UnsafeCell` pour accéder à la mutabilité interne
+        &mut *(self as *const _ as *mut FreeListAllocator)
+    }
+
     /// Cherche un bloc libre correspondant à une taille et un alignement donnés
     pub unsafe fn find_block(&mut self, size: usize, alignment: usize) -> Option<(*mut Block, usize)> {
         let mut current_block = *self.free_list.get();
@@ -114,6 +119,26 @@ impl FreeListAllocator {
 
         (*new_block).next = *self.free_list.get();
         *self.free_list.get() = new_block;
+    }
+
+    /// Fusion des  sb blocs libres adjacents
+    pub unsafe fn merge_free_blocks(&mut self) {
+        let mut current_block = *self.free_list.get();
+
+        while !current_block.is_null() {
+            let next_block = (*current_block).next;
+
+            if (*current_block).finishing_addr() && !next_block.is_null() == next_block as usize {
+                
+                (*current_block).size += (*next_block).size;
+                
+                (*current_block).next = (*next_block).next;
+            } else {
+                
+                
+                current_block = next_block;
+            }
+        }
     }
 
     /// Initialise l'allocateur
